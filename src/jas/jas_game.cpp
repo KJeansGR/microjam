a951d85dc5b05a64b39aae656fbadc0419031e33
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-03-05 12:00:55 -0800

    Added credits namespace

--- src/jas/jas_game.cpp
+++ src/jas/jas_game.cpp
@@ -6,3 +6,12 @@
 #include "bn_sprite_animate_actions.h"
 
 #include "mj/mj_game_list.h"
+
+
+namespace
+{
+    constexpr bn::string_view code_credits[] = { "Aaron, Jamison, Seadrah" };
+    constexpr bn::string_view graphics_credits[] = { "" };
+    constexpr bn::string_view sfx_credits[] = {""};
+    constexpr bn::string_view music_credits[] = {""};
+}
