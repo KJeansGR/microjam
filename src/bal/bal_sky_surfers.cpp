e7fe045c1ac13de0ff138f9fb2e37c513aa1de62
Pasha <pavloyousee@gmail.com>
2026-03-08 05:10:19 +0000

    wave4 compleated

--- src/bal/bal_sky_surfers.cpp
+++ src/bal/bal_sky_surfers.cpp
@@ -1,5 +1,24 @@
 #include "bal_sky_surfers.h"
 
+
+#include "mj/mj_game_list.h"
+
+
+
+namespace
+{
+    constexpr bn::string_view code_credits[] = { "Pasha, Hosea" };
+    constexpr bn::string_view graphics_credits[] = { "" };
+    constexpr bn::string_view sfx_credits[] = {""};
+    constexpr bn::string_view music_credits[] = {""};
+}
+
+MJ_GAME_LIST_ADD(bal::bal_sky_surfers)
+MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
+MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
+MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
+MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
+
 namespace bal
 {
 bal_sky_surfers::bal_sky_surfers([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
@@ -20,6 +39,10 @@ mj::game_result bal_sky_surfers::play([[maybe_unused]] const mj::game_data& data
 
     mj::game_result result(victory(), false);
     return result;
+}
+
+bool bal_sky_surfers::victory() const {
+    return 0;
 }
 
 void bal_sky_surfers::fade_in([[maybe_unused]] const mj::game_data& data)
